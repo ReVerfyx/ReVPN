8becb36b94008d16b1e9b34d240df5d27ad90ceb
ReVerfyx Studio <blinchikiooo5@gmail.com>
2026-09-21 19:36:35 +0300

    Generate unique IDs for multiple VPS servers

--- server/install.sh
+++ server/install.sh
@@ -6,7 +6,7 @@ if [[ "${EUID}" -ne 0 ]]; then
   exit 1
 fi
 
-SERVER_NAME="${SERVER_NAME:-Сервер #1}"
+SERVER_NAME="${SERVER_NAME:-Мой VPS}"
 COUNTRY="${COUNTRY:-Нидерланды}"
 CITY="${CITY:-Амстердам}"
 PUBLIC_HOST="${PUBLIC_HOST:-}"
