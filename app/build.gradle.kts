872c96f1b443fe91a7d4dd7d76251426160c6796
ReVerfyx Studio <blinchikiooo5@gmail.com>
2026-09-21 20:00:49 +0300

    Build ReVPN 0.2 account release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.reverfyx.revpn"
         minSdk = 21
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.2.0"
         buildConfigField("String", "GOOGLE_WEB_CLIENT_ID", "\"$googleWebClientId\"")
     }
